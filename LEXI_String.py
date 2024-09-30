454f5b31b0ff7fc2ad7508001a11fc2aa56841a3
Shubhranshu <143248748+Shubhranshu331@users.noreply.github.com>
2024-09-30 19:52:38 +0530

    Update LEXI_String.py

--- LEXI_String.py
+++ LEXI_String.py
@@ -6,11 +6,19 @@ while (test>0):
   for i in s:
     list.append(p.find(i))
 
-list.sort()
-for i in list:
-  print(p[i],end = '')
+    list.sort()
+    for i in list:
+        print(p[i],end = '')
 
-if test >1:
-  print ()
+    if test >1:
+        print ()
 
-test= -1
+    test= -1
+#alternate method
+for i in range(int(input())):
+    p=[x for x in input()]
+    s=[x for x in input()]
+    for x in p :
+        if(x in s):
+            print(x,end="")
+    print()
